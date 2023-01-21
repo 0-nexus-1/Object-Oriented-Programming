1971315e0a5e5eceb456a6e286be166ec7b58229
༺༒Nexu$༒༻ <123191758+0-nexus-1@users.noreply.github.com>
2023-01-21 18:40:21 +0500

    Create composition-class.cpp

---
+++ Classes/composition-class.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+
+class Engine
+{
+private:
+    int capacity;
+
+public:
+    Engine()
+    {
+        capacity = 0;
+        cout << "DCtor of Engine\n";
+    }
+
+    Engine(int c)
+    {
+        capacity = c;
+        cout << "PCtor of Engine\n";
+    }
+
+    void display()
+    {
+        cout << "Capacity: " << capacity << endl;
+    }
+};
+
+class Car
+{
+private:
+    int model;
+    Engine *e[3];
+
+public:
+    Car()
+    {
+        model = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            e[i] = new Engine();
+        }
+        cout << "DCtro of Car\n";
+    }
+
+    Car(int m, Engine *e1[])
+    {
+        model = m;
+        for (int i = 0; i < 3; i++)
+        {
+            e[i] = e1[i];
+        }
+        cout << "PCtro of Car\n";
+    }
+
+    void display()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            e[i]->display(); // Engine
+        }
+    }
+};
+
+int main()
+{
+    Engine *engarr[3];
+    engarr[0] = new Engine(800);
+    engarr[1] = new Engine(1000);
+    engarr[2] = new Engine(1300);
+
+    Car *c = new Car(2019, engarr);
+
+    c->display();
+}
